fe6bb430d85cc8a3096e59786f8b3cc9a87021a7
Logosh <rr.david.marlor@gmail.com>
2021-11-15 10:54:38 +0000

    Final Exercise

--- main.py
+++ main.py
@@ -39,3 +39,14 @@ a = b
 b = aux
 print("a = "+ a)
 print("b = "+ b)
+
+#Final exercise:
+#1. Create a greeting for your program.
+#2. Ask the user for the city that they frew up in.
+#3. Ask the user for the name of a pet.
+#4. Combine the name of their city and pet and show them their band name.
+#5. Make sure the input cursor shows on a new line.
+print("Welcome to the Band Name Generator.")
+city = input("What's name of the city you grew up in?\n")
+pet = input("What's yout pet's name?\n")
+print("Your band name could be "+ city + " "+ pet)
