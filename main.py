c077a8460b0e7c5f46ce17f399f85cbf6c831294
Logosh <rr.david.marlor@gmail.com>
2021-11-15 10:35:33 +0000

    Input Function

--- main.py
+++ main.py
@@ -10,3 +10,12 @@ print('Day 1 - String Manipulation')
 print('String Concatenation is done with the "+" sign')
 print('e.g. print ("Hello " + "world")')
 print('New lines can be created with a backslash and n.')
+#Funcion Input:
+#Diferentes formas de usar input
+print("\nUsando print para preguntar")
+print("What is your name?\n")
+print("Usando input como entrada de datos")
+input("What is your name?\n")
+
+#De esta forma primero se ejecuta el input y luego se incluye el nombre en el Hello
+print("\nHello "+ input("What is your name?"))
