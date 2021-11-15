6ec2e1995f34b07db4b452d4ff4dc6601d9badbc
Logosh <rr.david.marlor@gmail.com>
2021-11-15 10:29:10 +0000

    Exercise 2

--- main.py
+++ main.py
@@ -4,4 +4,9 @@ print("print('what to print')")
 print("\n")
 #atajos
 print("Hello World!\nHello World!")
-print("\nHello"+" David")
+print("\nHello"+" David\n")
+#Ejercicio 2 Si hay que introducir dentro del print "" o '' usar el opuesto para el print
+print('Day 1 - String Manipulation')
+print('String Concatenation is done with the "+" sign')
+print('e.g. print ("Hello " + "world")')
+print('New lines can be created with a backslash and n.')
