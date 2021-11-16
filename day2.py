0c47a654199e0ca4273ad13ab46115a8a9035a65
Logosh <rr.david.marlor@gmail.com>
2021-11-16 10:06:09 +0000

    Reorganize

---
+++ day2.py
@@ -0,0 +1,9 @@
+print("Este es el día 2")
+
+#Ejercicio 1
+#Write a program that adds the digits in a 2 digit number e.g. input 35 the output 3 +5 = 8
+a = input("Introduce un numero: \n")
+b = int(a[0])
+c = int(a[1])
+print("La suma de "+a+" es: ",(b+c))
+
