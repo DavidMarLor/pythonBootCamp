1454209df6a262c30a77222e37424af11e42884a
Logosh <rr.david.marlor@gmail.com>
2021-11-16 10:31:20 +0000

    Exercise 2

--- day2.py
+++ day2.py
@@ -1,9 +1,18 @@
 print("Este es el día 2")
 
-#Ejercicio 1
+#Exercise 1
 #Write a program that adds the digits in a 2 digit number e.g. input 35 the output 3 +5 = 8
-a = input("Introduce un numero: \n")
-b = int(a[0])
-c = int(a[1])
-print("La suma de "+a+" es: ",(b+c))
+#a = input("Introduce un numero: \n")
+#b = int(a[0])
+#c = int(a[1])
+#print("La suma de "+a+" es: ",(b+c))
+
+#Exercise 2 
+#Calculate BMI = w/h
+height = input("Enter your height in m: ")
+weight = input("Enter your weight in kg: ")
+
+bmi = int(weight) / float(height) **2
+print(int(bmi))
+
 
