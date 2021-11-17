0dbb498897673cc027c703d18f435b492fff6fb1
Logosh <rr.david.marlor@gmail.com>
2021-11-17 12:18:25 +0000

    Exercise 3

--- day3/day3.py
+++ day3/day3.py
@@ -48,7 +48,7 @@ else:
 Write a program that works out whether if a given year is a leap year. Every year that is 
 evenly divisible by 4, except every year that is divisible by 100 unless the year is also 
 divisible by 400
-"""
+
 year = int(input("Which year do you want to check?\n"))
 messageY = f"The year {year} is a leap year"
 messageN = f"The year {year} is not a leap year"
@@ -62,3 +62,50 @@ if (year % 4) == 0:
     print(messageY)
 else:
   print(messageN)
+"""
+#Exercise 3
+"""
+Small Pizza = $15
+Medium Pizza = $20
+Large Pizza = $25
+
+Pepperoni for small pizza = $2
+Pepperoni for medium/large pizza = $3
+
+Extra cheese for any pizza = $1
+"""
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M or L \n")
+add_pepperoni = input("Do you want pepperoni? Y or N \n")
+extra_cheese = input("Do you want extra cheese? Y or N \n")
+bill = 0
+
+if size == 'S':
+  bill += 15
+  if add_pepperoni == 'Y':
+    bill += 2
+    if extra_cheese == 'Y':
+      bill += 1
+  else:
+    if extra_cheese == 'Y':
+      bill += 1
+elif size == 'M':
+  bill += 20
+  if add_pepperoni == 'Y':
+    bill += 3
+    if extra_cheese == 'Y':
+      bill += 1
+  else:
+    if extra_cheese == 'Y':
+      bill += 1
+else:
+  bill += 25
+  if add_pepperoni == 'Y':
+    bill += 3
+    if extra_cheese == 'Y':
+      bill += 1
+  else:
+    if extra_cheese == 'Y':
+      bill += 1
+
+print("Your final bill is: ", bill)
