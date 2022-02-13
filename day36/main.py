c5c6f5b81a47f57f69b71d2f2efea67b9210d9b7
David <rr.david.marlor@gmail.com>
2022-02-13 12:20:15 +0100

    day36
    todo sms sends

---
+++ day36/main.py
@@ -0,0 +1,88 @@
+import requests
+import datetime
+
+STOCK_NAME = "TSLA"
+COMPANY_NAME = "Tesla Inc"
+API_KEY_ALPHA = "1I8I40DRWNL5IX0M"
+API_KEY_NEWS = "229ce5c6b753438799daba76fbed10af"
+
+STOCK_ENDPOINT = "https://www.alphavantage.co/query"
+NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
+
+ACTUAL_YEAR = str(datetime.datetime.now().year)
+ACTUAL_MONTH = str(datetime.datetime.now().month)
+TODAY = str(datetime.datetime.now().day)
+YESTERDAY = str(datetime.datetime.now().day-1)
+
+param_request_alpha = {
+    "function": "TIME_SERIES_DAILY",
+    "symbol": STOCK_NAME,
+    "apikey": API_KEY_ALPHA
+}
+
+param_request_news = {
+    "q": STOCK_NAME,
+    "apikey": API_KEY_NEWS
+}
+
+# STEP 1: Use https://www.alphavantage.co/documentation/#daily
+# When stock price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
+
+alpha = requests.get(url=STOCK_ENDPOINT, params=param_request_alpha)
+news = requests.get(url=NEWS_ENDPOINT, params=param_request_news)
+
+# TODO 1. - Get yesterday's closing stock price. Hint: You can perform list comprehensions on Python dictionaries.
+#  e.g. [new_value for (key, value) in dictionary.items()]
+data = alpha.json()["Time Series (Daily)"]["2022-02-10"]["4. close"]
+
+# TODO 2. - Get the day before yesterday's closing stock price
+data2 = alpha.json()["Time Series (Daily)"]["2022-02-09"]["4. close"]
+
+# TODO 3. - Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20.
+#  Hint: https://www.w3schools.com/python/ref_func_abs.asp
+difference = abs(float(data) - float(data2))
+
+# TODO 4. - Work out the percentage difference in price between closing price yesterday and closing price the day
+#  before yesterday.
+percentage = (difference/float(data))
+
+# TODO 5. - If TODO4 percentage is greater than 5 then print("Get News").
+if (percentage*100) > 5:
+    print("Get News")
+else:
+    print("Not so far")
+
+# STEP 2: https://newsapi.org/
+# Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
+
+# TODO 6. - Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
+data_news = news.json()
+
+# TODO 7. - Use Python slice operator to create a list that contains the first 3 articles. Hint:
+#  https://stackoverflow.com/questions/509211/understanding-slice-notation
+list_news = data_news["articles"][:3]
+
+# STEP 3: Use twilio.com/docs/sms/quickstart/python
+# to send a separate message with each article's title and description to your phone number.
+
+# TODO 8. - Create a new list of the first 3 article's headline and description using list comprehension.
+final_list = []
+article_complete = {}
+for article in list_news:
+    article_complete["headline"] = article["title"]
+    article_complete["description"] = article["description"]
+    final_list.append(article_complete)
+print(final_list)
+
+# TODO 9. - Send each article as a separate message via Twilio.
+
+# Optional TODO: Format the message like this:
+"""
+TSLA: 🔺2% Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. Brief: We at Insider Monkey have 
+gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings 
+show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash. 
+or "TSLA: 🔻5% Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. Brief: We at Insider Monkey 
+have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F 
+filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus 
+market crash. 
+"""
